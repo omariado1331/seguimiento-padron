af33162d7dec7cc885a825a285e02573f507ca4b
flavio abdon <flaviocondori@gmail.com>
2025-11-12 06:22:48 -0400

    Remove Kit and Proceso serializers
    Removed unused serializers for Kit and Proceso.

--- operaciones/serializers.py
+++ operaciones/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import (
-    Kit, Llave, Ruta, Proceso, Estacion, 
+    Llave, Ruta, Estacion, 
     MovimientosEstacion, Coordinador, Operador,
     ReporteDiario, RegistroDespliegue, Item)
 
@@ -63,11 +63,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'groups']
-
-class KitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Kit
-        fields = '__all__'
     
 class LlaveSerializer(serializers.ModelSerializer):
     class Meta:
@@ -77,11 +72,6 @@ class LlaveSerializer(serializers.ModelSerializer):
 class RutaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ruta
-        fields = '__all__'
-
-class ProcesoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Proceso
         fields = '__all__'
 
 class EstacionSerializer(serializers.ModelSerializer):

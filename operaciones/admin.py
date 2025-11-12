70c61ee497b16d181584fdc6434a8bbbbe1d42b9
flavio abdon <flaviocondori@gmail.com>
2025-11-12 11:02:50 -0400

    Update search fields in EstacionAdmin
    Removed 'codigo_estacion' from search fields in EstacionAdmin.

--- operaciones/admin.py
+++ operaciones/admin.py
@@ -11,7 +11,7 @@ admin.site.register(Ruta)
 @admin.register(Estacion)
 class EstacionAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Estacion._meta.fields]
-    search_fields = ['codigo_estacion', 'nro_estacion', 'llave', 'fase', 'estado']
+    search_fields = [ 'nro_estacion', 'llave', 'fase', 'estado']
     list_filter = ['estado_computadora', 'fase']
     
 #admin.site.register(MovimientosEstacion)

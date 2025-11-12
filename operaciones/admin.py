fb87255273cde57dc3693bcf8fce33566df9d1fc
flavio abdon <flaviocondori@gmail.com>
2025-11-12 06:15:24 -0400

    Update admin registration for models
    Removed registration of Kit and MovimientosEstacion models in admin.

--- operaciones/admin.py
+++ operaciones/admin.py
@@ -1,22 +1,20 @@
 from django.contrib import admin
 from .models import (
-    Kit, Llave, Ruta, Proceso, Estacion, 
+    Llave, Ruta,  Estacion, 
     MovimientosEstacion, Coordinador, Operador,
     ReporteDiario, RegistroDespliegue, Item)
 
 # Register your models here.
 
-admin.site.register(Kit)
 admin.site.register(Llave)
 admin.site.register(Ruta)
-admin.site.register(Proceso)
 @admin.register(Estacion)
 class EstacionAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Estacion._meta.fields]
     search_fields = ['codigo_estacion', 'etiqueta_kit', 'nro_estacion', 'llave', 'fase', 'estado']
     list_filter = ['estado_computadora', 'fase']
     
-admin.site.register(MovimientosEstacion)
+#admin.site.register(MovimientosEstacion)
 admin.site.register(Coordinador)
 admin.site.register(Operador)
 admin.site.register(ReporteDiario)

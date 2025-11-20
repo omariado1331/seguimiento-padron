a943a89c8eb13db7f097e8be3986274f893e2912
flavio abdon <flaviocondori@gmail.com>
2025-11-20 11:18:39 -0400

    Update URL configuration with new API endpoints
    Added API routes and views for user management and deployment records.

--- seguimiento_padron/urls.py
+++ seguimiento_padron/urls.py
@@ -1,23 +1,50 @@
-"""
-URL configuration for seguimiento_padron project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
+from .views import CustomTokenObtainPairView
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView
+from .api_views import UltimoRegistroDespliegueView 
+from . import views
+from .api_views import (
+    LlaveViewSet, RutaViewSet, 
+    EstacionViewSet, MovimientosEstacionViewSet, CoordinadorViewSet, 
+    OperadorViewSet, ReporteDiarioViewSet, RegistroDespliegueViewSet,
+    UserViewSet, ListarOperadoresListView, ItemViewSet, 
+    CentroEmpadronamientoViewSet, UbicacionesOperadorViewSet,
+    ListarPuntosEmpadronamientoView
+)
+from . import views
+
+from .views import mapa_dashboard
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'llaves', LlaveViewSet)
+router.register(r'rutas', RutaViewSet)
+router.register(r'estaciones', EstacionViewSet)
+router.register(r'movimientos-estacion', MovimientosEstacionViewSet)
+router.register(r'coordinadores', CoordinadorViewSet)
+router.register(r'operadores', OperadorViewSet)
+router.register(r'reportesdiarios', ReporteDiarioViewSet)
+router.register(r'registrosdespliegue', RegistroDespliegueViewSet)
+router.register(r'items', ItemViewSet)
+router.register(r'centros-empadronamiento', CentroEmpadronamientoViewSet)
+router.register(r'ubicaciones-operador', UbicacionesOperadorViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('operaciones.urls')),
+    #path('', views.index, name='index'),
+    #path para apis
+    path('api/', include(router.urls)),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('listar-operadores/', ListarOperadoresListView.as_view(), name='listado-operadores'),
+    path('api/ultimo-registro-despliegue/<int:id_operador>/',
+         UltimoRegistroDespliegueView.as_view(),
+         name='ultimo-registro-despliegue'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('panel/<str:rol>/', views.panel_view, name='panel'),
+    path('qr/scan/', views.qr_scan, name='qr_scan'),
+    path('api/listar-puntos-empadronamiento/',
+         ListarPuntosEmpadronamientoView.as_view(),
+         name='listar-puntos-empadronamiento'),
+    path('mapa/', mapa_dashboard, name='mapa_dashboard'), 
 ]

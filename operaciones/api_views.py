d6f0e7bf1648a0e21c95ab6f68f87c3e08eb99d6
omariado1331 <omariadocalle1331@gmail.com>
2025-11-28 12:29:35 -0400

    panel de asignadas

--- operaciones/api_views.py
+++ operaciones/api_views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.decorators import action
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import RegistroDespliegue
@@ -17,6 +18,11 @@ from .models import (
     UbicacionesOperador
 )
 from simple_history.utils import update_change_reason
+from reportlab.pdfgen import canvas
+from reportlab.lib.utils import ImageReader
+from reportlab.lib.pagesizes import landscape, A5
+import qrcode
+from io import BytesIO
 from .serializers import (
     LlaveSerializer, RutaSerializer,
     EstacionSerializer, MovimientosEstacionSerializer, 
@@ -51,6 +57,74 @@ class EstacionViewSet(viewsets.ModelViewSet):
         user = self.request.user if self.request.user.is_authenticated else None
         obj = serializer.save(_history_user=user)
         update_change_reason(obj, "Modificado desde Panel")
+    
+    # @action(detail=True, methods=["get"])
+    # def ticket(self, request, pk=None):
+    #     estacion = self.get_object()
+    #     llave = estacion.llave
+    #     codigo_equipo = estacion.codigo_equipo
+    #     buffer = BytesIO()
+    #     pdf = canvas.Canvas(buffer, pagesize=landscape(A5))
+
+    #     tickets = [
+    #         (20, 210),
+    #         (220, 210),
+    #         (20, 20),
+    #         (220, 20),
+    #     ]
+
+    #     for x,y in tickets:
+    #         self.draw_ticket(pdf, llave, codigo_equipo, x, y)
+
+    #     pdf.showPage()
+        
+    #     pdf.save()
+    #     buffer.seek(0)
+
+    #     return HttpResponse(buffer.getvalue(), content_type='application/pdf')
+    
+    # def draw_ticket(self, pdf, llave, codigo_equipo, x, y):
+    #     #dimensiones del ticket
+    #     width = 180
+    #     height = 100
+
+    #     #  dibujar borde del ticket
+    #     pdf.setLineWidth(1)
+    #     pdf.rect(x, y, width, height)
+
+    #      # --- Logos ---
+    #     logo_left = ImageReader("operaciones/static/img/oep.png")
+    #     logo_right = ImageReader("operaciones/static/img/sereci.png")
+        
+    #     pdf.drawImage(logo_left, x + 5, y + height - 40, width=35, height=35, preserveAspectRatio=True)
+    #     pdf.drawImage(logo_right, x + width - 40, y + height - 40, width=35, height=35, preserveAspectRatio=True)
+
+    #     # --- Título centrado ---
+    #     pdf.setFont("Helvetica-Bold", 6)
+    #     pdf.drawCentredString(x + width/2, y + height - 20, "TRIBUNAL SUPREMO ELECTORAL")
+    #     pdf.setFont("Helvetica", 5)
+    #     pdf.drawCentredString(x + width/2, y + height - 28, "SERVICIO DE REGISTRO CIVICO - LA PAZ")
+    #     pdf.drawCentredString(x + width/2, y + height - 33, "EMPADRONAMIENTO BIOMETRICO MASIVO")
+    #     pdf.drawCentredString(x + width/2, y + height - 38, "ELECCION DE AUTORIDADES DEPARTAMENTALES, REGIONALES Y MUNICIPALES")
+    #     pdf.drawCentredString(x + width/2, y + height - 43, "SUBNACIONALES 2026")
+
+    #     # --- Número de estación ---
+    #     pdf.setFont("Helvetica-Bold", 32)
+    #     pdf.drawCentredString(x + width/2, y + height/2 -10 , str(llave.nro_estacion))
+
+    #     # --- Contadores ---
+    #     pdf.setFont("Helvetica-Bold", 12)
+    #     pdf.drawString(x , y - 10 , f"R: {llave.contador_r}    C: {llave.contador_c}")
+
+    #     # --- QR ---
+    #     qrData = f"{llave.nro_estacion}|R:{llave.contador_r}|C:{llave.contador_c}|{codigo_equipo}"
+    #     qr = qrcode.make(qrData)
+    #     qr_io = BytesIO()
+
+    #     qr.save(qr_io, format="PNG")
+    #     qr_io.seek(0)
+    #     qr_image = ImageReader(qr_io)
+    #     pdf.drawImage(qr_image, x+100, y, width=50, height=50)
 
 
 class MovimientosEstacionViewSet(viewsets.ModelViewSet):
